ff9a7027866fa0f9d621fcc866f34c2a80677da2
Adrian Marin <adrian@adrianthedev.com>
2021-08-27 15:03:29 +0300

    fix next-auth ts error

---
+++ types/next-auth.d.ts
@@ -0,0 +1,11 @@
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `Provider` React Context
+   */
+  interface Session {
+    user: {
+      firstName: string
+      lastName: string
+    }
+  }
+}
