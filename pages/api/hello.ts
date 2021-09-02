ea99492ac5c0934db71b76423f45385a303b8897
Adrian Marin <adrian@adrianthedev.com>
2021-09-02 17:39:37 +0300

    wrap hello function in sentry handler

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,14 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withSentry } from '@sentry/nextjs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<Data>
-) {
+) => {
   if (req.query.error) throw new Error('Api error.')
   res.status(200).json({ name: 'John Doe' })
 }
+
+export default withSentry(handler)
