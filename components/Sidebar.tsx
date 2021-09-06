c2aaec8df44d25e07546fda70b96fb2561c85e2e
Adrian Marin <adrian@adrianthedev.com>
2021-09-06 12:35:46 +0300

    better copy when removing a data source

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,15 @@
 import { TrashIcon } from "@heroicons/react/outline";
 import { toast } from "react-toastify";
-import { useGetDataSourceQuery, useRemoveDataSourceMutation } from "@/features/data-sources/api-slice";
+import {
+  useGetDataSourceQuery,
+  useRemoveDataSourceMutation,
+} from "@/features/data-sources/api-slice";
 import { useGetTablesQuery } from "@/features/tables/api-slice";
 import { useRouter } from "next/router";
-import LoadingOverlay from "./LoadingOverlay"
+import LoadingOverlay from "./LoadingOverlay";
 import React, { memo } from "react";
 import SidebarItem from "./SidebarItem";
-import isEmpty from "lodash/isEmpty"
+import isEmpty from "lodash/isEmpty";
 
 const Sidebar = () => {
   const router = useRouter();
@@ -30,20 +33,21 @@ const Sidebar = () => {
     skip: !dataSourceId,
   });
 
-  const [removeDataSource, {isLoading: dataSourceIsRemoving}] = useRemoveDataSourceMutation()
+  const [removeDataSource, { isLoading: dataSourceIsRemoving }] =
+    useRemoveDataSourceMutation();
 
   const handleRemove = async () => {
     if (dataSourceIsLoading || dataSourceIsRemoving) return;
 
     const confirmed = confirm(
-      "Adre you sure you want to remove this datasource?"
+      "Are you sure you want to remove this data source? All information about it (settings included) will be completely removed from our servers."
     );
     if (confirmed) {
       toast(
         "The data source has been removed. You will be redirected to the homepage. Thank you!"
       );
 
-      await removeDataSource({dataSourceId})
+      await removeDataSource({ dataSourceId });
 
       await setTimeout(async () => {
         await router.push("/");
@@ -66,12 +70,15 @@ const Sidebar = () => {
               className="mt-1 flex items-center text-xs text-gray-600 cursor-pointer"
               onClick={handleRemove}
             >
-              <TrashIcon className="h-4 inline" /> {dataSourceIsRemoving ? 'removing' : 'remove'}
+              <TrashIcon className="h-4 inline" />{" "}
+              {dataSourceIsRemoving ? "removing" : "remove"}
             </a>
           </div>
         )}
         {error && <div>Error: {(error as any).error}</div>}
-        {isLoading && <LoadingOverlay transparent={isEmpty(tablesResponse?.data)} />}
+        {isLoading && (
+          <LoadingOverlay transparent={isEmpty(tablesResponse?.data)} />
+        )}
         {/* @todo: why does the .data attribute remain populated with old content when the hooks has changed? */}
         {/* Got to a valid DS and then to an invalid one. the data attribute will still have the old data there. */}
         {tablesResponse?.ok &&
