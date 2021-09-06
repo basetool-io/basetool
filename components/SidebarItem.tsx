5ae76255b07ee6c26ce8af4c876100daa70a4835
Adrian Marin <adrian@adrianthedev.com>
2021-09-06 13:08:02 +0300

    minor visual tweak

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -10,7 +10,7 @@ function SidebarItem({ active, label, link }: { active?: boolean; label: string;
     >
       <a
         className={classNames(
-          "hover:bg-blue-gray-100 overflow-hidden overflow-ellipsis max-w-full",
+          "hover:bg-blue-gray-100 overflow-hidden overflow-ellipsis w-full",
           "relative flex flex-grow-0 text-gray-800 font-normal cursor-pointer text-sm py-2 px-4 rounded-md leading-none m-0",
           { "bg-blue-gray-200 hover:bg-gray-200": active }
         )}
