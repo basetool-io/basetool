4372f9507e5feff263aa58e6e54e2df3378a069c
Adrian Marin <adrian@adrianthedev.com>
2021-09-08 15:00:12 +0300

    fix inspector edit issue

--- plugins/fields/Association/Inspector.tsx
+++ plugins/fields/Association/Inspector.tsx
@@ -1,7 +1,11 @@
-import { Code, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { Code, FormControl, FormLabel, Input, Select } from "@chakra-ui/react";
 import { Column } from "@/features/fields/types";
+import { isUndefined } from "lodash"
+import { useBoolean } from "react-use"
+import { useGetColumnsQuery } from "@/features/tables/api-slice"
+import { useRouter } from "next/router"
 import OptionWrapper from "@/features/tables/components/OptionsWrapper";
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import fieldOptions from "./fieldOptions";
 
 function Inspector({
@@ -11,27 +15,47 @@ function Inspector({
   column: Column;
   setColumnOption: (c: Column, name: string, value: any) => void;
 }) {
-  const initialValue =
-    column.fieldOptions.nameColumn || fieldOptions.nameColumn;
+  const router = useRouter()
+  const initialValue = useMemo(
+    () => (column.fieldOptions.nameColumn as string) || fieldOptions.nameColumn,
+    [column.fieldOptions.nameColumn, fieldOptions.nameColumn]
+  );
+  const [editRaw, toggleEditRaw] = useBoolean(false)
+
+  // fetch the column for that foreign table ahed of time to better show the user what fields he can choose
+  const dataSourceId = router.query.dataSourceId as string
+  const tableName = column.foreignKeyInfo.foreignTableName
+  const {
+    data: columnsResponse,
+    error,
+    isLoading,
+  } = useGetColumnsQuery(
+    {
+      dataSourceId,
+      tableName,
+    },
+    { skip: !dataSourceId || !tableName }
+  );
 
   // when changing the field type to this one, the new options are not automatically passed to the column
   useEffect(() => {
     setColumnOption(column, "fieldOptions.nameColumn", initialValue);
   }, []);
 
+  useEffect(() => {
+    toggleEditRaw(!isUndefined(error))
+  }, [error]);
+
   return (
     <OptionWrapper helpText="Make it easier to your users to identify the record you are referencing with this foreign key.">
       <FormControl id="nameColumn">
         <FormLabel>
-          Foregin key <Code>name</Code> column
+          <Code>name</Code> column for this association
         </FormLabel>
-        <Input
+
+        {!editRaw && !error && !isLoading && columnsResponse?.ok && (<Select
           className="font-mono"
-          type="text"
-          name="nameColumn"
-          placeholder="Name column"
-          required={false}
-          defaultValue={initialValue as number}
+          value={initialValue}
           onChange={(e) => {
             setColumnOption(
               column,
@@ -39,7 +63,27 @@ function Inspector({
               e.currentTarget.value
             );
           }}
-        />
+        >
+          {columnsResponse.data && columnsResponse.data.map((column: Column) => <option value={column.name}>{column.name}</option>)}
+        </Select>)}
+
+        {editRaw && <Input
+          className="font-mono"
+          type="text"
+          name="nameColumn"
+          placeholder="Name column"
+          required={false}
+          value={initialValue}
+          onChange={(e) => {
+            setColumnOption(
+              column,
+              "fieldOptions.nameColumn",
+              e.currentTarget.value
+            );
+          }}
+        />}
+
+        <a className="text-xs cursor-pointer text-blue-600" onClick={toggleEditRaw}>Edit raw value</a>
       </FormControl>
     </OptionWrapper>
   );
