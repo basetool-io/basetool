48957a49cacb064ff6540df0bcac5de7b627b22f
Adrian Marin <adrian@adrianthedev.com>
2021-09-10 14:20:42 +0300

    console log sentry dsn

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -5,6 +5,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+console.log('SENTRY_DSN->', SENTRY_DSN)
 
 Sentry.init({
   dsn: SENTRY_DSN,
