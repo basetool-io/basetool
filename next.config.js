545b2a364307c0d55c167e2b82ecb8004268f70a
Adrian Marin <adrian@adrianthedev.com>
2021-09-10 13:46:30 +0300

    try without silent

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ const SentryWebpackPluginOptions = {
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
 
-  silent: true, // Suppresses all logs
+  // silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
