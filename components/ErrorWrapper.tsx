80331a05ff7c560eba2a07a5017ea06adab001e2
Adrian Marin <adrian@adrianthedev.com>
2021-09-16 17:02:44 +0300

    fix tsc errror

--- components/ErrorWrapper.tsx
+++ components/ErrorWrapper.tsx
@@ -1,20 +1,35 @@
-import IApiResponse from "@/features/api/ApiResponse";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
+import { IApiResponse } from "@/features/api/ApiResponse"
+import { SerializedError } from "@reduxjs/toolkit";
 import PageWrapper from "./PageWrapper";
-import React from "react";
+import React, { useMemo } from "react";
 
-function ErrorWrapper({ error }: { error: IApiResponse }) {
-  return (
+function ErrorWrapper({
+  error,
+}: {
+  error: FetchBaseQueryError | SerializedError;
+}) {
+  const errorData = useMemo(() => 'data' in error ? error?.data as IApiResponse : undefined, [error]);
+
+return (
     <PageWrapper heading="An error has occured.">
       <div className="flex-1 flex flex-col justify-between">
         <div className="space-y-4">
           <div>
             <div className="font-bold text-sm uppercase">Error message:</div>
-            {error?.data?.messages.join(' ')}
+            {errorData?.messages && errorData?.messages?.join(" ")}
+            {'message' in error && error?.message && error?.message}
           </div>
-          {error?.data?.meta?.links && <div>
-            <div className="font-bold text-sm uppercase">Helpful links:</div>
-            <div>{error?.data?.meta?.links.map((link: string) => <a href={link}>{link}</a>)}</div>
-          </div>}
+          {errorData?.meta?.links && (
+            <div>
+              <div className="font-bold text-sm uppercase">Helpful links:</div>
+              <div>
+                {errorData?.meta?.links.map((link: string) => (
+                  <a href={link}>{link}</a>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
         {/* <div>Link to docs</div> */}
       </div>
