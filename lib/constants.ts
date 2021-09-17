bdf36c33fcbeffd9e86bf4c84d1837ca75577b21
Adrian Marin <adrian@adrianthedev.com>
2021-09-17 18:31:03 +0300

    remove subdomain blacklist

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,19 +1,1 @@
 export const LOCAL_STORAGE_PREFIX = "basetool";
-
-export const SUBDOMAINS_BLACKLIST = [
-  "www",
-  "app",
-  "api",
-  "docs",
-  "documentation",
-  "dev",
-  "developers",
-  "buy",
-  "purchase",
-  "login",
-  "register",
-  "signin",
-  "hq",
-  "avocado",
-  "avo",
-];
