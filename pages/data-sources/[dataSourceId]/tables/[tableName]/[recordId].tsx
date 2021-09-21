eec09b9e63c92d4bf7bd7a6b2162c11396b59189
mihaimdm <mihai.mdm2007@gmail.com>
2021-09-21 11:47:51 +0300

    remove duplicate code

--- pages/data-sources/[dataSourceId]/tables/[tableName]/[recordId].tsx
+++ pages/data-sources/[dataSourceId]/tables/[tableName]/[recordId].tsx
@@ -4,7 +4,6 @@ import { EyeIcon, PencilAltIcon } from "@heroicons/react/outline";
 import { Views } from "@/features/fields/enums";
 import { getField } from "@/features/fields/factory";
 import { makeField } from "@/features/fields";
-import { useAccessControl } from "@/hooks";
 import { useGetColumnsQuery } from "@/features/tables/api-slice";
 import { useGetRecordQuery } from "@/features/records/api-slice";
 import { useRouter } from "next/router";
@@ -17,67 +16,6 @@ import PageWrapper from "@/components/PageWrapper";
 import React, { useMemo } from "react";
 import isArray from "lodash/isArray";
 import isEmpty from "lodash/isEmpty";
-import type { Record } from "@/features/records/types";
-
-const RecordShow = ({
-  record,
-  columns,
-}: {
-  record: Record;
-  columns: Column[];
-}) => {
-  const router = useRouter();
-  const backLink = useMemo(() => {
-    if (router.query.fromTable) {
-      if (router.query.fromRecord) {
-        return `/data-sources/${router.query.dataSourceId}/tables/${router.query.fromTable}/${router.query.fromRecord}`;
-      } else {
-        return `/data-sources/${router.query.dataSourceId}/tables/${router.query.fromTable}`;
-      }
-    }
-
-    return `/data-sources/${router.query.dataSourceId}/tables/${router.query.tableName}`;
-  }, [router.query]);
-  const ac = useAccessControl();
-
-  return (
-    <>
-      <PageWrapper
-        heading="View record"
-        buttons={
-          <>
-            <ButtonGroup size="sm">
-              <BackButton href={backLink} />
-              { ac.updateAny("record").granted && <>
-              <Link
-                href={`/data-sources/${router.query.dataSourceId}/tables/${router.query.tableName}/${record.id}/edit`}
-                passHref
-              >
-                <Button colorScheme="blue">Edit</Button>
-              </Link>
-              </> }
-            </ButtonGroup>
-          </>
-        }
-      >
-        <>
-          {columns &&
-            record &&
-            columns.map((column: Column) => {
-              const field = makeField({
-                record,
-                column,
-                tableName: router.query.tableName as string,
-              });
-              const Element = getField(column, Views.show);
-
-              return <Element key={column.name} field={field} />;
-            })}
-        </>
-      </PageWrapper>
-    </>
-  );
-};
 
 function RecordsShow() {
   const router = useRouter();
