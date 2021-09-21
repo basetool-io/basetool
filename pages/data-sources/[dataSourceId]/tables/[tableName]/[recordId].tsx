41eacffd9ab00d9a7c45f498c843e5f7ca17065c
mihaimdm <mihai.mdm2007@gmail.com>
2021-09-21 11:45:49 +0300

    redactor record page

--- pages/data-sources/[dataSourceId]/tables/[tableName]/[recordId].tsx
+++ pages/data-sources/[dataSourceId]/tables/[tableName]/[recordId].tsx
@@ -1,6 +1,6 @@
 import { Button, ButtonGroup } from "@chakra-ui/button";
 import { Column } from "@/features/fields/types";
-import { EyeIcon, PencilAltIcon } from "@heroicons/react/outline";
+import { EyeIcon, PencilAltIcon, TrashIcon } from "@heroicons/react/outline";
 import { Views } from "@/features/fields/enums";
 import { getField } from "@/features/fields/factory";
 import { makeField } from "@/features/fields";
@@ -16,88 +16,7 @@ import PageWrapper from "@/components/PageWrapper";
 import React, { useMemo } from "react";
 import isArray from "lodash/isArray";
 import isEmpty from "lodash/isEmpty";
-import type { Record } from "@/features/records/types";
 
-const RecordShow = ({
-  record,
-  columns,
-}: {
-  record: Record;
-  columns: Column[];
-}) => {
-  const router = useRouter();
-  const backLink = useMemo(() => {
-    if (router.query.fromTable) {
-      if (router.query.fromRecord) {
-        return `/data-sources/${router.query.dataSourceId}/tables/${router.query.fromTable}/${router.query.fromRecord}`;
-      } else {
-        return `/data-sources/${router.query.dataSourceId}/tables/${router.query.fromTable}`;
-      }
-    }
-
-    return `/data-sources/${router.query.dataSourceId}/tables/${router.query.tableName}`;
-  }, [router.query]);
-
-  const [deleteRecord, { isLoading: isDeleting }] = useDeleteRecordMutation();
-
-  const handleDelete = async () => {
-    const confirmed = confirm(
-      "Are you sure you want to remove this record?"
-    );
-    if (confirmed) {
-      const response = await deleteRecord({
-        dataSourceId: router.query.dataSourceId as string,
-        tableName: router.query.tableName as string,
-        recordId: record.id.toString(),
-      });
-
-      if ("data" in response && response?.data?.ok) {
-        // @todo: make the response into a pretty message
-      }
-
-      router.push(backLink);
-    }
-  }
-
-  return (
-    <>
-      <PageWrapper
-        heading="View record"
-        buttons={
-          <>
-            {isDeleting && <LoadingOverlay transparent={true} />}
-            <ButtonGroup size="sm">
-              <BackButton href={backLink} />
-              <Button colorScheme="red" onClick={handleDelete}>Delete</Button>
-              <Link
-                href={`/data-sources/${router.query.dataSourceId}/tables/${router.query.tableName}/${record.id}/edit`}
-                passHref
-              >
-                <Button colorScheme="blue">Edit</Button>
-              </Link>
-            </ButtonGroup>
-          </>
-        }
-      >
-        <>
-          {columns &&
-            record &&
-            columns.map((column: Column) => {
-              const field = makeField({
-                record,
-                column,
-                tableName: router.query.tableName as string,
-              });
-              const Element = getField(column, Views.show);
-
-              return <Element key={column.name} field={field} />;
-            })}
-        </>
-      </PageWrapper>
-    </>
-  );
-};
-  
 function RecordsShow() {
   const router = useRouter();
   const dataSourceId = router.query.dataSourceId as string;
@@ -144,6 +63,24 @@ function RecordsShow() {
     return `/data-sources/${router.query.dataSourceId}/tables/${router.query.tableName}`;
   }, [router.query]);
 
+
+  const [deleteRecord, { isLoading: isDeleting }] = useDeleteRecordMutation();
+
+  const handleDelete = async () => {
+    const confirmed = confirm(
+      "Are you sure you want to remove this record?"
+    );
+    if (confirmed) {
+      await deleteRecord({
+        dataSourceId: router.query.dataSourceId as string,
+        tableName: router.query.tableName as string,
+        recordId: record.id.toString(),
+      });
+
+      router.push(backLink);
+    }
+  }
+
   return (
     <>
       <Layout>
@@ -160,8 +97,10 @@ function RecordsShow() {
               flush={true}
               buttons={
                 <>
+                  {isDeleting && <LoadingOverlay transparent={true} />}
                   <ButtonGroup size="sm">
                     <BackButton href={backLink} />
+                    <Button colorScheme="red" leftIcon={<TrashIcon className="h-4" />} onClick={handleDelete}>Delete</Button>
                     <Link
                       href={`/data-sources/${router.query.dataSourceId}/tables/${router.query.tableName}/${record.id}/edit`}
                       passHref
