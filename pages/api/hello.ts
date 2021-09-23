67b179568e46464edbb8cc9f4c07ab8d0a9079e2
Adrian Marin <adrian@adrianthedev.com>
2021-09-24 00:26:35 +0300

    bring back error

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { withSentry } from '@sentry/nextjs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default async function handler(
+const handler = (
   req: NextApiRequest,
   res: NextApiResponse<Data>
-) {
+) => {
+  if (req.query.error) throw new Error('Server errror')
+
   res.status(200).json({ name: 'John Doe' })
 }
+
+export default withSentry(handler)
