854593b9053c24a6626a13446fe05e1540283d40
mihaimdm <mihai.mdm2007@gmail.com>
2021-09-23 11:42:52 +0300

    remove link from id

--- plugins/fields/Id/Index.tsx
+++ plugins/fields/Id/Index.tsx
@@ -1,19 +1,9 @@
 import { Field } from "@/features/fields/types";
-import { useRouter } from "next/router";
 import IndexFieldWrapper from "@/features/fields/components/FieldWrapper/IndexFieldWrapper";
-import Link from "next/link";
 import React, { memo } from "react";
 
-const Index = ({ field }: { field: Field }) => {
-  const router = useRouter();
-  const dataSourceId = router.query.dataSourceId as string;
-  const tableName = router.query.tableName as string;
-
-  const link = `/data-sources/${dataSourceId}/tables/${tableName}/${field.value}`;
-
-  return (
-    <IndexFieldWrapper field={field}><Link href={link}>{field.value}</Link></IndexFieldWrapper>
-  )
-};
+const Index = ({ field }: { field: Field }) => (
+  <IndexFieldWrapper field={field}>{field.value}</IndexFieldWrapper>
+);
 
 export default memo(Index);
