aaa3d8ad3748c3e0f321d7b29fe851ba115c911e
Adrian Marin <adrian@adrianthedev.com>
2021-09-28 17:28:09 +0300

    home page tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,56 @@
-import DataSourcesBlock from "@/features/data-sources/components/DataSourcesBlock"
+import { Code } from "@chakra-ui/react";
+import { useGetDataSourcesQuery } from "@/features/data-sources/api-slice";
+import DataSourcesBlock from "@/features/data-sources/components/DataSourcesBlock";
 import Layout from "@/components/Layout";
-import OrganizationsBlock from "@/features/organizations/components/OrganizationsBlock"
+import Link from "next/link";
+import OrganizationsBlock from "@/features/organizations/components/OrganizationsBlock";
 import PageWrapper from "@/components/PageWrapper";
 import ProfileContext from "@/lib/ProfileContext";
-import React, { useContext } from "react";
+import React, { useContext, useMemo, useState } from "react";
 
 function Index() {
   const { user } = useContext(ProfileContext);
+
+  const {
+    data: dataSourcesResponse,
+    isLoading,
+    error,
+  } = useGetDataSourcesQuery();
+  const [genericProjects, setGenericProjects] = useState<
+    {
+      name: string;
+      credentials: string;
+      description: string;
+      isVisible: boolean;
+    }[]
+  >([
+    {
+      name: "Car rental",
+      credentials: process.env.NEXT_PUBLIC_DUMMY_1_DB_CREDENTIALS as string,
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. A amet fugiat blanditiis nihil! Eaque quos, fuga dolorum tenetur beatae repellat. Culpa iste similique libero facilis sed quidem accusamus? Esse, doloribus.",
+      isVisible: false,
+    },
+    {
+      name: "CRM",
+      credentials: process.env.NEXT_PUBLIC_DUMMY_2_DB_CREDENTIALS as string,
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. A amet fugiat blanditiis nihil! Eaque quos, fuga dolorum tenetur beatae repellat. Culpa iste similique libero facilis sed quidem accusamus? Esse, doloribus.",
+      isVisible: false,
+    },
+    {
+      name: "Ticketing",
+      credentials: process.env.NEXT_PUBLIC_DUMMY_3_DB_CREDENTIALS as string,
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. A amet fugiat blanditiis nihil! Eaque quos, fuga dolorum tenetur beatae repellat. Culpa iste similique libero facilis sed quidem accusamus? Esse, doloribus.",
+      isVisible: false,
+    },
+  ]);
+
+  const hasDataSources = useMemo(
+    () => dataSourcesResponse?.ok && dataSourcesResponse.data.length > 0,
+    [dataSourcesResponse]
+  );
 
   return (
     <Layout hideSidebar={true}>
@@ -14,6 +58,58 @@ function Index() {
         <>
           <DataSourcesBlock />
           <OrganizationsBlock />
+
+          <PageWrapper.Section>
+            <PageWrapper.Heading>Test databases</PageWrapper.Heading>
+            <div className="text-md">
+              If you don't yet want to add your data source at the moment but
+              want to test how things work, use a dummy database to see how fast
+              you can get up and running.
+            </div>
+            <div className="divide-y mt-4">
+              {genericProjects &&
+                Object.values(genericProjects).map(
+                  ({ name, credentials, description, isVisible }, idx) => {
+                    return (
+                      <div className="block py-4" key={idx}>
+                        <span className="text-xl font-extralight">
+                          {idx + 1}. {name}
+                        </span>
+                        {isVisible || (
+                          <a
+                            className="ml-1 text-blue-600 cursor-pointer text-sm"
+                            onClick={() => {
+                              const newProjects = [...genericProjects];
+                              // toggle the isVisible property
+                              newProjects[idx].isVisible =
+                                !newProjects[idx].isVisible;
+                              setGenericProjects(newProjects);
+                            }}
+                          >
+                            (show credentials)
+                          </a>
+                        )}
+                        <div>
+                          {isVisible && (
+                            <>
+                              <Code>{credentials}</Code>{" "}
+                              <Link
+                                href={`/data-sources/postgresql/new?credentials=${credentials}&name=${name}`}
+                              >
+                                <a className="ml-1 text-blue-600 cursor-pointer text-sm">
+                                  (use these credentials)
+                                </a>
+                              </Link>
+                            </>
+                          )}
+                        </div>
+                        {/* <div className="mt-4">{description}</div> */}
+                      </div>
+                    );
+                  }
+                )}
+            </div>
+          </PageWrapper.Section>
         </>
       </PageWrapper>
     </Layout>
