172889caf954dcf4a8ab792a06ed1bb084a28e72
Adrian Marin <adrian@adrianthedev.com>
2021-09-28 13:22:52 +0300

    refactor: datetime localized

--- plugins/fields/DateTime/Index.tsx
+++ plugins/fields/DateTime/Index.tsx
@@ -1,9 +1,16 @@
+import { DateTime } from "luxon";
 import { Field } from "@/features/fields/types";
+import { dateTimeFormat, getBrowserTimezone } from "@/lib/time";
 import IndexFieldWrapper from "@/features/fields/components/FieldWrapper/IndexFieldWrapper";
 import React, { memo } from "react";
 
-const Show = ({ field }: { field: Field }) => (
-  <IndexFieldWrapper field={field}>{field.value}</IndexFieldWrapper>
-);
+const Show = ({ field }: { field: Field }) => {
+  const date = DateTime.fromISO(field.value as string);
+  const formattedDate = date
+    .setZone(getBrowserTimezone())
+    .toFormat(dateTimeFormat);
+
+  return <IndexFieldWrapper field={field}>{formattedDate}</IndexFieldWrapper>;
+};
 
 export default memo(Show);
--- plugins/fields/DateTime/Show.tsx
+++ plugins/fields/DateTime/Show.tsx
@@ -1,9 +1,16 @@
+import { DateTime } from "luxon";
 import { Field } from "@/features/fields/types";
+import { dateTimeFormat, getBrowserTimezone } from "@/lib/time";
 import React, { memo } from "react";
 import ShowFieldWrapper from "@/features/fields/components/FieldWrapper/ShowFieldWrapper";
 
-const Show = ({ field }: { field: Field }) => (
-  <ShowFieldWrapper field={field}>{field.value}</ShowFieldWrapper>
-);
+const Show = ({ field }: { field: Field }) => {
+  const date = DateTime.fromISO(field.value as string);
+  const formattedDate = date
+    .setZone(getBrowserTimezone())
+    .toFormat(dateTimeFormat);
+
+  return <ShowFieldWrapper field={field}>{formattedDate}</ShowFieldWrapper>;
+};
 
 export default memo(Show);
