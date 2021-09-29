36f905f0aeaf3c65c1d1d3cb8db8c1b19f02c6f0
Adrian Marin <adrian@adrianthedev.com>
2021-09-29 16:33:14 +0300

    fix form and schema

--- plugins/data-sources/postgresql/schema.ts
+++ plugins/data-sources/postgresql/schema.ts
@@ -2,8 +2,10 @@ import Joi from "joi";
 
 export const schema = Joi.object({
   name: Joi.string().min(3).required(),
+  type: Joi.string().allow('postgresql', 'googlesheets').required(),
   credentials: Joi.object({
     url: Joi.string().required(),
     useSsl: Joi.boolean(),
   }),
+  organizationId: Joi.number().required()
 });
