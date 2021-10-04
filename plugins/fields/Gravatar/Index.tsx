ebed55337083558f0eb5d92cae5f7526ed3b17e9
Marian Dumitru <d.mariandev@gmail.com>
2021-10-04 09:30:36 +0300

    .gitignore fix

--- plugins/fields/Gravatar/Index.tsx
+++ plugins/fields/Gravatar/Index.tsx
@@ -4,14 +4,15 @@ import { isNull } from "lodash";
 import IndexFieldWrapper from "@/features/fields/components/FieldWrapper/IndexFieldWrapper";
 import React, { memo } from "react";
 import Image from "next/image";
-import { Md5 } from "ts-md5";
 
 const Index = ({ field }: { field: Field }) => {
   const value = isNull(field.value) ? <Code>null</Code> : field.value;
+  const md5 = require('md5');
+
 
   return <IndexFieldWrapper field={field}>
         <Image
-          src={`https://www.gravatar.com/avatar/${Md5.hashStr(value as string)}`}
+          src={`https://www.gravatar.com/avatar/${md5(value as string)}`}
           width={field.column.fieldOptions.indexWidth as number}
           height={field.column.fieldOptions.indexHeight as number}
           alt={value as string}
