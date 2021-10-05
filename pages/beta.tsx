61bb0b9f54e16682a4768ff59dee845b0c08f9a0
Adrian Marin <adrian@adrianthedev.com>
2021-10-05 22:53:54 +0300

    better beta page

--- pages/beta.tsx
+++ pages/beta.tsx
@@ -7,7 +7,42 @@ function Beta() {
     <Layout hideSidebar={true}>
       <PageWrapper heading="Beta">
         <>
-          Basetool is currently in beta. If you run into issues, don't hesitate to contact us.
+          <div className="block">
+            Basetool is currently in alpha. You are some of the first users to
+            try it out, and we thank you for that. Myself (
+            <a
+              href="https://twitter.com/adrianthedev"
+              target="_blank"
+              rel="noreferrer"
+              title="Adrian Marin"
+              className="underline"
+            >
+              Adrian
+            </a>
+            ) and my brother (
+            <a
+              href="https://twitter.com/mihaimdm"
+              target="_blank"
+              rel="noreferrer"
+              title="Mihai David Marin"
+              className="underline"
+            >
+              David
+            </a>
+            ) are waiting for your feedback.
+            <br />
+            <br />
+            If you run into any issues, don't
+            hesitate to contact us at{" "}
+            <a className="inline underline" href="mailto:adrian@basetool.io">
+              adrian@basetool.io
+            </a>{" "}
+            and{" "}
+            <a className="inline underline" href="mailto:david@basetool.io">
+              david@basetool.io
+            </a>{" "}
+            or through Intercom 👇
+          </div>
         </>
       </PageWrapper>
     </Layout>
