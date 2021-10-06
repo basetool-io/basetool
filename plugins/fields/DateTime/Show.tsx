2a3a8046f4486575e13857e1bb804500e133ee6b
Adrian Marin <adrian@adrianthedev.com>
2021-10-06 17:18:13 +0300

    fix: date time field errored on create

--- plugins/fields/DateTime/Show.tsx
+++ plugins/fields/DateTime/Show.tsx
@@ -1,17 +1,21 @@
 import { Code } from "@chakra-ui/layout";
-import { DateTime } from "luxon";
 import { Field } from "@/features/fields/types";
 import { dateFormat, dateTimeFormat, getBrowserTimezone } from "@/lib/time";
 import { isNull } from "lodash";
+import { parsed } from "./parsedValue";
 import React, { memo } from "react";
 import ShowFieldWrapper from "@/features/fields/components/FieldWrapper/ShowFieldWrapper";
 
 const Show = ({ field }: { field: Field }) => {
-  const formattedDate = DateTime.fromISO(field.value as string)
+  const formattedDate = parsed(field.value)
     .setZone(getBrowserTimezone())
     .toFormat(field.column.fieldOptions.onlyDate ? dateFormat : dateTimeFormat);
 
-  return <ShowFieldWrapper field={field}>{isNull(field.value) ? <Code>null</Code> : formattedDate}</ShowFieldWrapper>;
+  return (
+    <ShowFieldWrapper field={field}>
+      {isNull(field.value) ? <Code>null</Code> : formattedDate}
+    </ShowFieldWrapper>
+  );
 };
 
 export default memo(Show);
