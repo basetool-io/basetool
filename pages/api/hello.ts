5003912bf0a1ec8bb5f1a63f33bcd31b9a66fdac
Adrian Marin <adrian@adrianthedev.com>
2021-10-06 19:18:04 +0300

    add email to hello

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,13 @@
 import { withMiddlewares } from "@/features/api/middleware";
+import email from "@/lib/email"
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await email.send({
+    to: ["adrian@basetool.io", "david@basetool.io"],
+    subject: "New user signup",
+    text: `New user with email ${'payload.email'} and organization ${'payload.organization'}`,
+  });
   res.status(200).json({ name: "John Doe" });
 };
 
