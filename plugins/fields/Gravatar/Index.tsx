fb8dc3a9b4713b25d198cefb3bb0b5fc80b3378d
Marian Dumitru <d.mariandev@gmail.com>
2021-10-10 13:55:45 +0300

    feature/progress-bar-field - empty field wrappers for gravatar field - shouldn't be on this branch

--- plugins/fields/Gravatar/Index.tsx
+++ plugins/fields/Gravatar/Index.tsx
@@ -1,12 +1,11 @@
 import { Field } from "@/features/fields/types";
-import { Md5 } from "ts-md5";
-import Image from 'next/image';
+
 import IndexFieldWrapper from "@/features/fields/components/FieldWrapper/IndexFieldWrapper";
 import React, { memo } from "react";
 
 const Index = ({ field }: { field: Field }) => (
   <IndexFieldWrapper field={field}>
-    <Image src={"https://www.gravatar.com/avatar/" + Md5.hashStr(field.value as string)} alt="gravatar" />
+    <div></div>
   </IndexFieldWrapper>
 );
 
--- plugins/fields/Gravatar/Show.tsx
+++ plugins/fields/Gravatar/Show.tsx
@@ -1,12 +1,10 @@
 import { Field } from "@/features/fields/types";
-import { Md5 } from "ts-md5";
-import Image from 'next/image';
 import React, { memo } from "react";
 import ShowFieldWrapper from "@/features/fields/components/FieldWrapper/ShowFieldWrapper";
 
 const Show = ({ field }: { field: Field }) => (
   <ShowFieldWrapper field={field}>
-    <Image src={"https://www.gravatar.com/avatar/" + Md5.hashStr(field.value as string)} alt="gravatar" />
+    <div></div>
   </ShowFieldWrapper>
 );
 
