c908a575097ebfcdc3b08211bb4dc4338a679760
Marian Dumitru <d.mariandev@gmail.com>
2021-10-16 17:03:37 +0300

    feature/progress-bar - removed commented lines from schema

--- plugins/fields/ProgressBar/schema.ts
+++ plugins/fields/ProgressBar/schema.ts
@@ -5,14 +5,6 @@ import type { Record } from "@/features/records/types";
 const schema = (record: Record, column: Column) => {
   const rule = Joi.number().allow(null, "", NaN);
 
-  // rule = rule.allow(null)
-  // if (column.nullable && !column.required) {
-  //   console.log('nullable', column.name)
-  //   rule = rule.allow(null)
-  // } else {
-  //   rule = rule.required()
-  // }
-
   return rule;
 };
 
