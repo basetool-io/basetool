4f9fb89dbc70cca9ba54880ece9a7f410ca89dd9
Marian Dumitru <d.mariandev@gmail.com>
2021-10-16 17:15:17 +0300

    feature/progress-bar - small changes based on code review

--- plugins/fields/ProgressBar/Index.tsx
+++ plugins/fields/ProgressBar/Index.tsx
@@ -3,23 +3,25 @@ import { ProgressBarFieldOptions } from "./types";
 import IndexFieldWrapper from "@/features/fields/components/FieldWrapper/IndexFieldWrapper";
 import React, { memo } from "react";
 
-const Index = ({ field }: { field: Field }) => (
-  <IndexFieldWrapper field={field}>
-    <div
-      className="text-center text-sm font-semibold w-full leading-none mb-1"
-      hidden={
-        !(field.column.fieldOptions as ProgressBarFieldOptions).displayValue
-      }
-    >
-      {field.value}
-      {(field.column.fieldOptions as ProgressBarFieldOptions).valueSuffix}
-    </div>
-    <progress
-      max={(field.column.fieldOptions as ProgressBarFieldOptions).max}
-      value={field.value as number}
-      className="block w-full"
-    />
-  </IndexFieldWrapper>
-);
+const Index = ({ field }: { field: Field }) => {
+  const fieldOptions = field.column.fieldOptions as ProgressBarFieldOptions;
+
+  return (
+    <IndexFieldWrapper field={field}>
+      <div
+        className="text-center text-sm font-semibold w-full leading-none mb-1"
+        hidden={fieldOptions.displayValue}
+      >
+        {field.value}
+        {fieldOptions.valueSuffix}
+      </div>
+      <progress
+        max={fieldOptions.max}
+        value={field.value as number}
+        className="block w-full"
+      />
+    </IndexFieldWrapper>
+  );
+};
 
 export default memo(Index);
--- plugins/fields/ProgressBar/Show.tsx
+++ plugins/fields/ProgressBar/Show.tsx
@@ -3,23 +3,25 @@ import { ProgressBarFieldOptions } from "./types";
 import React, { memo } from "react";
 import ShowFieldWrapper from "@/features/fields/components/FieldWrapper/ShowFieldWrapper";
 
-const Show = ({ field }: { field: Field }) => (
-  <ShowFieldWrapper field={field}>
-    <div
-      className="text-center text-sm font-semibold w-full leading-none mb-1"
-      hidden={
-        !(field.column.fieldOptions as ProgressBarFieldOptions).displayValue
-      }
-    >
-      {field.value}
-      {(field.column.fieldOptions as ProgressBarFieldOptions).valueSuffix}
-    </div>
-    <progress
-      max={(field.column.fieldOptions as ProgressBarFieldOptions).max}
-      value={field.value as number}
-      className="block w-full"
-    />
-  </ShowFieldWrapper>
-);
+const Show = ({ field }: { field: Field }) => {
+  const fieldOptions = field.column.fieldOptions as ProgressBarFieldOptions;
+
+  return (
+    <ShowFieldWrapper field={field}>
+      <div
+        className="text-center text-sm font-semibold w-full leading-none mb-1"
+        hidden={!fieldOptions.displayValue}
+      >
+        {field.value}
+        {fieldOptions.valueSuffix}
+      </div>
+      <progress
+        max={fieldOptions.max}
+        value={field.value as number}
+        className="block w-full"
+      />
+    </ShowFieldWrapper>
+  );
+};
 
 export default memo(Show);
