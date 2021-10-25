f307feff14ab8e1cb149ad8a6ce9ff7be462ab43
David Mihai Marin <mihai.mdm2007@gmail.com>
2021-10-25 20:53:37 +0300

    [BST-149] fix: reorder columns sends old data for update (#148)
    Co-authored-by: Adrian Marin <adrian@adrianthedev.com>

--- features/tables/api-slice.ts
+++ features/tables/api-slice.ts
@@ -117,6 +117,26 @@ export const tablesApiSlice = createApi({
           { type: "Table", id: "LIST" },
         ],
       }),
+      updateColumnsOrder: builder.mutation<
+        ApiResponse,
+        Partial<{
+          dataSourceId: string;
+          tableName: string;
+          body: Record<string, unknown>;
+        }>
+      >({
+        query: ({ dataSourceId, tableName, body }) => ({
+          url: `${apiUrl}/data-sources/${dataSourceId}/tables/${tableName}/columns/order`,
+          method: "PUT",
+          body,
+        }),
+        invalidatesTags: (result, error, { dataSourceId, tableName }) => [
+          { type: "Table", id: dataSourceId },
+          { type: "Table", id: "LIST" },
+          { type: "TableColumns", id: tableName },
+          { type: "TableColumns", id: "LIST" },
+        ],
+      }),
     };
   },
 });
@@ -129,5 +149,6 @@ export const {
   useCreateColumnMutation,
   useUpdateTableMutation,
   useUpdateTablesMutation,
+  useUpdateColumnsOrderMutation,
   usePrefetch,
 } = tablesApiSlice;
