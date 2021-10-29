332e2fdeeae3d44d48ba01ca2ebdce3f63d419bc
mihaimdm <mihai.mdm2007@gmail.com>
2021-10-29 14:22:46 +0300

    cleanup

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,8 +2,6 @@ import { withMiddlewares } from "@/features/api/middleware";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.query.error) throw new Error('hey you')
-
   res.status(200).json({ name: "John Doe" });
 };
 
