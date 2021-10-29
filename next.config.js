a2c1837ba871384ac2c7282d75d9f1bf0b062514
Adrian Marin <adrian@adrianthedev.com>
2021-10-29 13:14:11 +0300

    update next config

--- next.config.js
+++ next.config.js
@@ -35,8 +35,10 @@ if (process.env.ANALYZE) {
 
   module.exports = withBundleAnalyzer(moduleExports);
 } else if (process.env.BASE_URL.includes('localhost')) {
-  module.exports = moduleExports
+  console.log(1)
+  module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
 } else {
+  console.log(2)
   // Make sure adding Sentry options is the last code to run before exporting, to
   // ensure that your source maps include changes from all other Webpack plugins
   module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
