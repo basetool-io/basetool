2fc2a3aa77f23fcb9da69ae75014735dd451ed72
mihaimdm <mihai.mdm2007@gmail.com>
2021-10-30 16:27:29 +0300

    remove raw value from association

--- plugins/fields/Association/Inspector.tsx
+++ plugins/fields/Association/Inspector.tsx
@@ -1,8 +1,6 @@
-import { Code, FormControl, FormLabel, Input, Select } from "@chakra-ui/react";
+import { Code, FormControl, FormLabel, Select } from "@chakra-ui/react";
 import { Column } from "@/features/fields/types";
-import { isUndefined } from "lodash";
 import { useDataSourceContext } from "@/hooks";
-import { useBoolean } from "react-use";
 import { useGetColumnsQuery } from "@/features/tables/api-slice";
 import OptionWrapper from "@/features/tables/components/OptionsWrapper";
 import React, { useEffect, useMemo } from "react";
@@ -19,7 +17,6 @@ function Inspector({
     () => (column.fieldOptions.nameColumn as string) || fieldOptions.nameColumn,
     [column.fieldOptions.nameColumn, fieldOptions.nameColumn]
   );
-  const [editRaw, toggleEditRaw] = useBoolean(false);
 
   // fetch the column for that foreign table ahed of time to better show the user what fields he can choose
   const { dataSourceId } = useDataSourceContext();
@@ -41,10 +38,6 @@ function Inspector({
     setColumnOptions(column, { "fieldOptions.nameColumn": initialValue });
   }, []);
 
-  useEffect(() => {
-    toggleEditRaw(!isUndefined(error));
-  }, [error]);
-
   return (
     <OptionWrapper helpText="Make it easier to your users to identify the record you are referencing with this foreign key.">
       <FormControl id="nameColumn">
@@ -52,7 +45,7 @@ function Inspector({
           <Code>name</Code> column for this association
         </FormLabel>
 
-        {!editRaw && !error && !isLoading && columnsResponse?.ok && (
+        { !error && !isLoading && columnsResponse?.ok && (
           <Select
             className="font-mono"
             value={initialValue}
@@ -68,29 +61,6 @@ function Inspector({
               ))}
           </Select>
         )}
-
-        {editRaw && (
-          <Input
-            className="font-mono"
-            type="text"
-            name="nameColumn"
-            placeholder="Name column"
-            required={false}
-            value={initialValue}
-            onChange={(e) => {
-              setColumnOptions(column, {
-                "fieldOptions.nameColumn": e.currentTarget.value,
-              });
-            }}
-          />
-        )}
-
-        <a
-          className="text-xs cursor-pointer text-blue-600"
-          onClick={toggleEditRaw}
-        >
-          Edit raw value
-        </a>
       </FormControl>
     </OptionWrapper>
   );
