daf6f59f2556b6482b5632948709f82424a2e67b
Adrian Marin <adrian@adrianthedev.com>
2021-10-31 20:26:27 +0200

    remove esmExternals

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 
 const moduleExports = {
+  experimental: {
+    esmExternals: false,
+  },
   images: {
     domains: ["www.gravatar.com"],
   },
@@ -34,11 +37,10 @@ if (process.env.ANALYZE) {
   });
 
   module.exports = withBundleAnalyzer(moduleExports);
-} else if (process.env.BASE_URL.includes('localhost')) {
+} else if (process.env.BASE_URL.includes("localhost")) {
   module.exports = moduleExports;
 } else {
   // Make sure adding Sentry options is the last code to run before exporting, to
   // ensure that your source maps include changes from all other Webpack plugins
   module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
 }
-
