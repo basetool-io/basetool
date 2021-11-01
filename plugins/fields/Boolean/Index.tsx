96e1bc69d513f8354f447f902b48dc23988847a4
Adrian Marin <adrian@adrianthedev.com>
2021-11-02 00:11:44 +0200

    fix: boolean field misaligned

--- plugins/fields/Boolean/Index.tsx
+++ plugins/fields/Boolean/Index.tsx
@@ -2,6 +2,7 @@ import { Code } from "@chakra-ui/layout";
 import { Field } from "@/features/fields/types";
 import { isNull } from "lodash";
 import BooleanCheck from "@/features/fields/components/BooleanCheck";
+import IndexFieldWrapper from "@/features/fields/components/FieldWrapper/IndexFieldWrapper"
 import React, { memo, useMemo } from "react";
 
 const Index = ({ field }: { field: Field }) => {
@@ -11,10 +12,10 @@ const Index = ({ field }: { field: Field }) => {
   );
 
   return (
-    <div className="flex items-center h-full">
+    <IndexFieldWrapper field={field}>
       {isNull(field.value) && <Code>null</Code>}
       {!isNull(field.value) && <BooleanCheck checked={isTruthy} />}
-    </div>
+    </IndexFieldWrapper>
   );
 };
 
