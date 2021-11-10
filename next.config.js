0c5a0af3c8059312f6949d81a8f5764f6c0a31e1
Adrian Marin <adrian@adrianthedev.com>
2021-11-10 21:10:15 +0200

    sentry ahck

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const moduleExports = {
   images: {
     domains: ["www.gravatar.com"],
   },
-  // outputFileTracing: false,
+  outputFileTracing: false,
   reactStrictMode: true,
   async redirects() {
     return [
