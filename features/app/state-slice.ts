3fa7c8bc6e8caeb2894ad25e44a12ebe1ca473bc
Adrian Marin <adrian@adrianthedev.com>
2021-11-11 10:49:35 +0200

    refactor: useDatasourceContext hook

--- features/app/state-slice.ts
+++ features/app/state-slice.ts
@@ -2,16 +2,10 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface AppState {
   sidebarsVisible: boolean;
-
-  dataSourceId: string;
-  tableName: string;
 }
 
 const initialState: AppState = {
   sidebarsVisible: false,
-
-  dataSourceId: '',
-  tableName: '',
 };
 
 const appStateSlice = createSlice({
@@ -26,26 +20,12 @@ const appStateSlice = createSlice({
     setSidebarVisibile(state, action: PayloadAction<boolean>) {
       state.sidebarsVisible = action.payload;
     },
-
-    setDataSourceId(state, action: PayloadAction<string>) {
-      state.dataSourceId = action.payload;
-    },
-
-    setTableName(state, action: PayloadAction<string>) {
-      state.tableName = action.payload;
-    },
   },
 });
 
 export const sidebarsVisibleSelector = ({ appState }: { appState: AppState }) =>
   appState.sidebarsVisible;
 
-export const dataSourceIdSelector = ({ appState }: { appState: AppState }) =>
-  appState.dataSourceId;
-
-export const tableNameSelector = ({ appState }: { appState: AppState }) =>
-  appState.tableName;
-
-export const { resetState, setSidebarVisibile, setDataSourceId, setTableName } = appStateSlice.actions;
+export const { resetState, setSidebarVisibile } = appStateSlice.actions;
 
 export default appStateSlice.reducer;
