fdf9f232ebe654ad80840a5c787b545fc5488a14
Adrian Marin <adrian@adrianthedev.com>
2021-11-14 18:21:45 +0200

    refactor: persistent sql connections

---
+++ plugins/data-sources/catalog.ts
@@ -0,0 +1,74 @@
+// import { PrismaClient } from '@prisma/client'
+
+import { DataSource } from "@prisma/client";
+import { QueryServiceWrapperPayload } from "./types";
+import QueryServiceWrapper from "./QueryServiceWrapper";
+
+export const getQueryServiceClass = async (type: string) => {
+  const dataSourceType = type === "maria_db" ? "mysql" : type;
+
+  return (
+    await import(`@/plugins/data-sources/${dataSourceType}/QueryService.ts`)
+  ).default;
+};
+
+type Connection = {
+  id: string;
+  type: string;
+  service: any;
+  createdAt: number
+};
+
+class Catalog {
+  public connections: Connection[] = [];
+
+  // constructor() {
+
+  // }
+
+  public async getConnection(
+    dataSource: DataSource,
+    payload: QueryServiceWrapperPayload
+  ) {
+    const id = dataSource.toString();
+    const existingConnection = this.connections.find(
+      (connection) => connection.id === id
+    );
+    if (existingConnection) {
+      console.log("existingConnection->", existingConnection);
+
+      return existingConnection.service;
+    }
+
+    console.log("no existing connection", id);
+    const queryServiceClass = await getQueryServiceClass(dataSource.type);
+
+    const service = new QueryServiceWrapper(queryServiceClass, payload);
+    this.connections.push({
+      id,
+      type: dataSource.type,
+      service,
+      createdAt: Date.now()
+    });
+    console.log("this.connections->", this.connections.length);
+
+    return service;
+  }
+}
+
+// add prisma to the NodeJS global type
+interface CustomNodeJsGlobal {
+  catalog: any;
+}
+
+// Prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal;
+// console.log("global.catalog->", global.catalog);
+
+// const catalog = global.catalog || new Catalog();
+const catalog = new Catalog();
+
+if (process.env.NODE_ENV === "development") global.catalog = catalog;
+console.log(1, 'catalog->', catalog);
+
+export default catalog;
--- plugins/data-sources/getQueryService.ts
+++ plugins/data-sources/getQueryService.ts
@@ -1,24 +1,33 @@
 import { IQueryServiceWrapper, QueryServiceWrapperPayload } from "./types";
-import QueryServiceWrapper from "./QueryServiceWrapper";
+import catalog from './catalog'
 
-export const getQueryServiceClass = async (type: string) => {
-  const dataSourceType = type === "maria_db" ? "mysql" : type;
+// export const getQueryServiceClass = async (type: string) => {
+//   const dataSourceType = type === "maria_db" ? "mysql" : type;
 
-  return (
-    await import(`@/plugins/data-sources/${dataSourceType}/QueryService.ts`)
-  ).default;
-};
+//   return (
+//     await import(`@/plugins/data-sources/${dataSourceType}/QueryService.ts`)
+//   ).default;
+// };
 
 const getQueryService = async (
   payload: QueryServiceWrapperPayload
 ): Promise<IQueryServiceWrapper> => {
-  let queryService;
+  // let queryService;
   const { dataSource } = payload;
+  // console.log('catalog->', catalog)
+  console.log('catalog->', catalog)
+
+
 
   try {
-    queryService = await getQueryServiceClass(dataSource.type);
+    // const queryService = await getQueryServiceClass(dataSource.type);
 
-    return new QueryServiceWrapper(queryService, payload);
+    // catalog.
+    const service = await catalog.getConnection(dataSource, payload)
+    // console.log(2, 'service->', service)
+    return service
+
+    // return catalog
   } catch (error: any) {
     if (error.code === "MODULE_NOT_FOUND") {
       // Returning a "null" Query service wrapper
