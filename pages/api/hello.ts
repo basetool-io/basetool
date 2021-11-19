98d1e2b0bff929a07c0b6477cee13e664c9adeef
Adrian Marin <adrian@adrianthedev.com>
2021-11-19 11:01:31 +0200

    cleanup

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,7 @@
 import { withMiddlewares } from "@/features/api/middleware";
-import prisma from "@/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const response = await prisma.dataSource.findMany({});
   res.status(200).json(response);
 };
 
