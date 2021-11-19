930c9b1b0605740968ea9ffabfa54612fde76321
Adrian Marin <adrian@adrianthedev.com>
2021-11-19 12:08:38 +0200

    chore: lint (#233)

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,7 +2,7 @@ import { withMiddlewares } from "@/features/api/middleware";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(response);
+  res.status(200).json({ hi: "there" });
 };
 
 export default withMiddlewares(handler);
