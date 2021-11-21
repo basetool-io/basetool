a07f8cadffc8f45355b16580f8ccc2841ff3600f
Adrian Marin <adrian@adrianthedev.com>
2021-11-21 14:32:40 +0200

    wip

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,12 @@
+import { isNull } from "lodash"
 import { withMiddlewares } from "@/features/api/middleware";
+import options from "@/features/options"
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(await options.set('runInProxy', 1))
+  const t = await options.get('runInProxy')
+  console.log(isNull(t))
   res.status(200).json({ hi: "there" });
 };
 
