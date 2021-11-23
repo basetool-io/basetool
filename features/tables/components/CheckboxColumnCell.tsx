b8d6a595b21b4df5ea69271efd0f31e9008c48db
David Mihai Marin <mihai.mdm2007@gmail.com>
2021-11-23 11:38:26 +0200

    [BST-237] fix: itemControls and checkbox appear when there is no record id (#246)

--- features/tables/components/CheckboxColumnCell.tsx
+++ features/tables/components/CheckboxColumnCell.tsx
@@ -1,9 +1,12 @@
 import { Checkbox } from "@chakra-ui/react";
+import { isUndefined } from "lodash";
 import { useSelectRecords } from "@/features/records/hooks";
 import React, { memo } from "react";
 
 const CheckboxColumnCell = ({ id }: { id: number }) => {
   const { selectedRecords, toggleRecordSelection } = useSelectRecords();
+
+  if (isUndefined(id)) return null;
 
   return (
     <div className="flex items-center justify-center h-full">
