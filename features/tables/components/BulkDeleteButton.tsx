9913726ddccf02ebd7f7115837a30b4349611106
David Mihai Marin <mihai.mdm2007@gmail.com>
2021-11-26 15:03:19 +0200

    [BST-248] fix: bulk delete disabled (#268)

--- features/tables/components/BulkDeleteButton.tsx
+++ features/tables/components/BulkDeleteButton.tsx
@@ -1,10 +1,9 @@
 import { Button, Tooltip } from "@chakra-ui/react";
 import { TrashIcon } from "@heroicons/react/outline";
-import { isNumber } from "lodash";
 import { useDataSourceContext } from "@/hooks";
 import { useDeleteBulkRecordsMutation } from "@/features/records/api-slice";
 import { useSelectRecords } from "@/features/records/hooks";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import pluralize from "pluralize";
 
 function BulkDeleteButton() {
@@ -13,37 +12,32 @@ function BulkDeleteButton() {
   const [deleteBulkRecords, { isLoading: isDeleting }] =
     useDeleteBulkRecordsMutation();
 
-  const [filteredSelectedRecords, setFilteredSelectedRecords] = useState<number[]>([]);
 
   const handleDeleteMultiple = async () => {
     if (
       confirm(
-        `Are you sure you want to remove ${filteredSelectedRecords.length} ${pluralize(
+        `Are you sure you want to remove ${selectedRecords.length} ${pluralize(
           "record",
-          filteredSelectedRecords.length
+          selectedRecords.length
         )}`
       )
     ) {
       await deleteBulkRecords({
         dataSourceId,
         tableName,
-        recordIds: filteredSelectedRecords as number[],
+        recordIds: selectedRecords as number[],
       });
     }
   };
 
   const deleteMessage = useMemo(
     () =>
-      `Delete ${filteredSelectedRecords.length} ${pluralize(
+      `Delete ${selectedRecords.length} ${pluralize(
         "record",
-        filteredSelectedRecords.length
+        selectedRecords.length
       )}`,
-    [filteredSelectedRecords.length]
+    [selectedRecords.length]
   );
-
-  useEffect(() => {
-    setFilteredSelectedRecords(selectedRecords.filter((id) => isNumber(id)));
-  }, [selectedRecords])
 
   return (
     <Tooltip label={deleteMessage} placement="bottom" gutter={10}>
@@ -53,12 +47,12 @@ function BulkDeleteButton() {
         colorScheme="red"
         leftIcon={<TrashIcon className="h-4" />}
         isLoading={isDeleting}
-        isDisabled={filteredSelectedRecords.length === 0}
+        isDisabled={selectedRecords.length === 0}
         onClick={handleDeleteMultiple}
       >
-        {filteredSelectedRecords.length > 0 && deleteMessage}
+        {selectedRecords.length > 0 && deleteMessage}
         {/* Add empty space 👇 so the icon doesn't get offset to the left when "Delete records" label is displayed */}
-        {filteredSelectedRecords.length === 0 && <>&nbsp;&nbsp;&nbsp;&nbsp;</>}
+        {selectedRecords.length === 0 && <>&nbsp;&nbsp;&nbsp;&nbsp;</>}
       </Button>
     </Tooltip>
   );
