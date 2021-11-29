765ecc0aaec254a54f4d9da53fc29c9bf6261809
Adrian Marin <adrian@adrianthedev.com>
2021-11-29 14:18:25 +0200

    [BST-256] fix: trim credentials for DS (#274)

--- pages/api/data-sources/check-connection.ts
+++ pages/api/data-sources/check-connection.ts
@@ -1,14 +1,19 @@
 import * as fs from "fs";
 import { HeartbeatResult } from "knex-utils/dist/lib/heartbeatUtils";
+import { SSHCredentials as ISSHCredentials } from "@/plugins/data-sources/types";
+import { MysqlCredentials } from "@/plugins/data-sources/mysql/types";
+import { PgCredentials } from "@/plugins/data-sources/postgresql/types";
+import { SQLDataSourceTypes } from "@/plugins/data-sources/abstract-sql-query-service/types";
 import { SSHConnectionError } from "@/lib/errors";
 import { SSHTunnelCredentials } from "@/plugins/data-sources/types";
 import { checkHeartbeat } from "knex-utils";
-import { first } from "lodash";
+import { first, isString } from "lodash";
 import { getKnexClient } from "@/plugins/data-sources/abstract-sql-query-service/getKnexClient";
 import {
   getOverrides,
   runInSSHTunnel,
 } from "@/plugins/data-sources/QueryServiceWrapper";
+import { trimValues } from "@/lib/helpers";
 import { withMiddlewares } from "@/features/api/middleware";
 import ApiResponse from "@/features/api/ApiResponse";
 import IsSignedIn from "@/features/api/middlewares/IsSignedIn";
@@ -33,7 +38,19 @@ const handler = async (
   }
 };
 
-async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
+
+type ParsedCredentials = {
+  type: SQLDataSourceTypes;
+  credentials: PgCredentials | MysqlCredentials;
+  options: Record<string, unknown>;
+  SSHCredentials: ISSHCredentials;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  files: any;
+};
+
+const parseCredentials = async (
+  req: NextApiRequest
+): Promise<ParsedCredentials> => {
   const form = formidable();
   const { fields, files } = await new Promise((resolve, reject) => {
     return form.parse(req, (error: any, fields: any, files: any) => {
@@ -43,13 +60,35 @@ async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
     });
   });
 
-  let response: HeartbeatResult = { isOk: false };
-
   // Parse and assign the credentials
   const type = fields.type;
-  const credentials = JSON.parse(fields.credentials);
-  const options = JSON.parse(fields.options);
-  let SSHCredentials = fields.ssh ? JSON.parse(fields.ssh) : {};
+
+  let credentials;
+  try {
+    credentials = JSON.parse(fields.credentials);
+  } catch {}
+
+  let options = {};
+  try {
+    options = trimValues(JSON.parse(fields.options));
+  } catch {}
+
+  const SSHCredentials = (
+    fields.ssh ? trimValues(JSON.parse(fields.ssh)) : {}
+  ) as ISSHCredentials;
+
+  // Cast port as int
+  if (isString(credentials.port)) credentials.port = parseInt(credentials.port);
+
+  return { type, credentials, options, SSHCredentials, files };
+};
+
+async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
+  const parsedCreds = await parseCredentials(req);
+  let { SSHCredentials } = parsedCreds;
+  const { type, credentials, options, files } = parsedCreds;
+
+  let response: HeartbeatResult = { isOk: false };
 
   if (options.connectsWithSSH) {
     // Get local port number
@@ -94,8 +133,6 @@ async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
       }
     }
   } else {
-    // Cast port as int
-    credentials.port = parseInt(credentials.port);
     // Get a new client
     const client = getKnexClient(type, credentials);
 
