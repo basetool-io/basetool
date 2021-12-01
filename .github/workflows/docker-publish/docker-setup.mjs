e93a8fb92ee28303572ecd55dafb88ea4517a1e0
Adrian Marin <adrian@adrianthedev.com>
2021-12-01 11:02:11 +0200

    wip

---
+++ .github/workflows/docker-publish/docker-setup.mjs
@@ -0,0 +1,19 @@
+#!/usr/bin/env zx
+import YAML from "yaml";
+import fs from "fs";
+
+const file = fs.readFileSync("./../../../deploy/docker/docker-compose.yml", "utf8");
+const parsed = YAML.parse(file);
+parsed.services.database = {
+  image: "postgres",
+  ports: ["5432:5432"],
+  environment: {
+    POSTGRES_USER: "basetool",
+    POSTGRES_PASSWORD: "basetool",
+    POSTGRES_DB: "basetool",
+  },
+};
+
+fs.writeFileSync("./../../../deploy/docker/docker-compose.yml", YAML.stringify(parsed));
+
+console.log(`The docker-compose.yml file has been updated 🎉`);
