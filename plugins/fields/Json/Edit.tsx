c65ca5a7cfec5bde47901538d5398d66b4d33210
Adrian Marin <adrian@adrianthedev.com>
2021-12-07 17:25:51 +0200

    [BST-268] fix: JSON field broken (#296)

--- plugins/fields/Json/Edit.tsx
+++ plugins/fields/Json/Edit.tsx
@@ -1,14 +1,28 @@
-import { EditFieldProps } from "@/features/fields/types";
 import {
+  Code,
   FormControl,
   FormErrorMessage,
   FormHelperText,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
   Textarea,
 } from "@chakra-ui/react";
+import { EditFieldProps } from "@/features/fields/types";
+import { InteractionProps } from "react-json-view";
 import { fieldId } from "@/features/fields";
-import { isEmpty, isNull, isUndefined } from "lodash";
+import { isEmpty, isNull, isString } from "lodash";
 import EditFieldWrapper from "@/features/fields/components/FieldWrapper/EditFieldWrapper";
-import React, { memo, useMemo, useState } from "react";
+import React, { memo, useEffect, useMemo, useState } from "react";
+import dynamic from "next/dynamic";
+
+const DynamicReactJson = dynamic(() => import("react-json-view"), {
+  ssr: false,
+});
+
+type JSONValues = Record<string, unknown> | [] | undefined | null;
 
 const Edit = ({
   field,
@@ -23,6 +37,7 @@ const Edit = ({
   const { name } = register;
 
   const [jsonError, setJsonError] = useState<string | null>(null);
+  const [localValue, setLocalValue] = useState<string>("");
 
   const hasError = useMemo(() => !isEmpty(errors[name]), [errors[name]]);
   const helpText = null;
@@ -39,68 +54,99 @@ const Edit = ({
     ? field.column.baseOptions.readonly
     : false;
 
-  let initialValue;
-  try {
-    initialValue =
-      isUndefined(field.value) || isNull(field.value)
-        ? isNull(defaultValue)
-          ? null
-          : JSON.stringify(JSON.parse(defaultValue as string), null, 2)
-        : JSON.stringify(JSON.parse(field.value as string), null, 2);
-  } catch (e) {
-    initialValue = null;
-  }
+  const value = useMemo(() => {
+    try {
+      if (isString(field.value)) {
+        return JSON.parse(field.value as string);
+      }
+    } catch (e) {
+      return null;
+    }
+
+    return field.value;
+  }, [field.value]);
 
   const handleOnChange = (value: string) => {
-    if (isEmpty(value)) {
-      if (setValue) {
-        setValue(
-          register.name,
-          {},
-          {
-            shouldValidate: true,
-            shouldDirty: true,
-            shouldTouch: true,
-          }
-        );
+    if (!isEmpty(value)) {
+      let parsedValue;
+
+      try {
+        parsedValue = JSON.parse(value);
+        setJsonError(null);
+      } catch (e) {
+        setJsonError("Error parsing the JSON contents!");
       }
-    } else {
-      if (setValue) {
-        let parsedValue;
-        try {
-          parsedValue = JSON.parse(value);
-          setJsonError(null);
-        } catch (e) {
-          setJsonError("Error parsing the JSON!");
-        }
-        if (parsedValue) {
-          setValue(register.name, parsedValue, {
-            shouldValidate: true,
-            shouldDirty: true,
-            shouldTouch: true,
-          });
-        }
-      }
+      setAllValues(parsedValue);
     }
   };
 
+  const setAllValues = (value: JSONValues) => {
+    if (setValue) {
+      setValue(register.name, value, {
+        shouldValidate: true,
+        shouldDirty: true,
+        shouldTouch: true,
+      });
+    }
+  };
+
+  useEffect(() => {
+    handleOnChange(localValue);
+  }, [localValue]);
+
+  useEffect(() => {
+    setLocalValue(JSON.stringify(value, null, 2));
+  }, []);
+
   return (
     <EditFieldWrapper field={field} schema={schema}>
-      <pre>{JSON.stringify(this, null, 2)}</pre>
       <FormControl
         isInvalid={hasError || !isNull(jsonError)}
         id={fieldId(field)}
         isDisabled={readonly}
       >
-        <Textarea
-          rows={10}
-          placeholder={placeholder as string}
-          id={fieldId(field)}
-          defaultValue={initialValue as string}
-          onChange={(e) => {
-            handleOnChange(e.currentTarget.value);
-          }}
-        />
+        <Tabs size="sm" variant="enclosed">
+          <TabList>
+            <Tab>Parsed</Tab>
+            <Tab>Raw</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              {isNull(value) && <Code>null</Code>}
+              {isNull(value) || (
+                <DynamicReactJson
+                  src={value as Record<string, unknown>}
+                  name={false}
+                  collapsed={false}
+                  displayObjectSize={true}
+                  displayDataTypes={true}
+                  enableClipboard={false}
+                  onEdit={(edit: InteractionProps) => {
+                    handleOnChange(JSON.stringify(edit.updated_src));
+                    setLocalValue(JSON.stringify(edit.updated_src, null, 2));
+                  }}
+                  onAdd={(edit: InteractionProps) => {
+                    handleOnChange(JSON.stringify(edit.updated_src));
+                    setLocalValue(JSON.stringify(edit.updated_src, null, 2));
+                  }}
+                />
+              )}
+            </TabPanel>
+            <TabPanel>
+              <Textarea
+                className="font-mono"
+                rows={10}
+                placeholder={placeholder as string}
+                id={fieldId(field)}
+                value={localValue}
+                onChange={(e) => {
+                  setLocalValue(e.currentTarget.value);
+                }}
+              />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
         {hasHelp && <FormHelperText>{helpText}</FormHelperText>}
         <FormErrorMessage>
           {errors[name]?.message || jsonError}
