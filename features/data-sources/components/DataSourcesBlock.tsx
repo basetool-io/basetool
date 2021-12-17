523ff7eb77d0e7e4dc7218e301aac63419987895
Adrian Marin <adrian@adrianthedev.com>
2021-12-17 15:34:13 +0200

    fix: no datasources state broken (#356)

--- features/data-sources/components/DataSourcesBlock.tsx
+++ features/data-sources/components/DataSourcesBlock.tsx
@@ -8,11 +8,62 @@ import PageWrapper from "@/components/PageWrapper";
 import React, { useMemo } from "react";
 import Shimmer from "@/components/Shimmer";
 
+const DataSourcesListing = ({
+  dataSources,
+  isLoading,
+}: {
+  dataSources: DataSource[];
+  isLoading: boolean;
+}) => {
+  const prefetchTables = usePrefetch("getTables");
+
+  return (
+    <PageWrapper.Blocks>
+      <>
+        {isLoading && (
+          <PageWrapper.Block href={`#`}>
+            <div className="text-lg font-bold text-gray-800 mb-2">Loading</div>
+            <br />
+            <Shimmer height={20} />
+          </PageWrapper.Block>
+        )}
+
+        {dataSources.map((dataSource: DataSource) => (
+          <PageWrapper.Block
+            href={`/data-sources/${dataSource.id}`}
+            key={dataSource.id}
+            onMouseOver={() =>
+              prefetchTables({
+                dataSourceId: dataSource.id.toString(),
+              })
+            }
+          >
+            <div className="relative flex flex-col justify-between h-full group">
+              <div className="text-lg font-bold text-gray-800 mb-2">
+                {dataSource.name}
+              </div>
+              <div className="absolute top-0 left-auto right-0">
+                <div className="relative min-w-[2rem] h-8 filter grayscale group-hover:grayscale-0">
+                  <Image
+                    src={`/img/logos/${dataSource.type}.png`}
+                    layout="fill"
+                    alt={`${dataSource.type} data source`}
+                  />
+                </div>
+              </div>
+              <br />
+            </div>
+          </PageWrapper.Block>
+        ))}
+      </>
+    </PageWrapper.Blocks>
+  );
+};
+
 const DataSourcesBlock = () => {
   const { data: dataSourcesResponse, isLoading: dataSourcesAreLoading } =
     useGetDataSourcesQuery();
   const { organizations, isLoading: profileIsLoading } = useProfile();
-  const prefetchTables = usePrefetch("getTables");
 
   const isLoading = useMemo(
     () => dataSourcesAreLoading || profileIsLoading,
@@ -21,15 +72,6 @@ const DataSourcesBlock = () => {
 
   return (
     <PageWrapper.Section>
-      {dataSourcesResponse?.data.length === 0 && (
-        <>
-          You don't have any Basetool data sources.{" "}
-          <Link href="/data-sources/new">
-            <a className="text-blue-600 underline">Add one</a>
-          </Link>{" "}
-          now.
-        </>
-      )}
       <div className="space-y-6">
         {!isLoading &&
           organizations.map((organization: Organization) => {
@@ -37,6 +79,8 @@ const DataSourcesBlock = () => {
               (dataSource: DataSource) =>
                 dataSource.organizationId === organization.id
             );
+
+            const hasDataSources = dataSources.length > 0;
 
             return (
               <div>
@@ -48,47 +92,22 @@ const DataSourcesBlock = () => {
 
                 <hr className="mb-4" />
 
-                <PageWrapper.Blocks>
+                {!hasDataSources && (
                   <>
-                    {isLoading && (
-                      <PageWrapper.Block href={`#`}>
-                        <div className="text-lg font-bold text-gray-800 mb-2">
-                          Loading
-                        </div>
-                        <br />
-                        <Shimmer height={20} />
-                      </PageWrapper.Block>
-                    )}
-
-                    {dataSources.map((dataSource: DataSource) => (
-                      <PageWrapper.Block
-                        href={`/data-sources/${dataSource.id}`}
-                        key={dataSource.id}
-                        onMouseOver={() => {
-                          prefetchTables({
-                            dataSourceId: dataSource.id.toString(),
-                          });
-                        }}
-                      >
-                        <div className="relative flex flex-col justify-between h-full group">
-                          <div className="text-lg font-bold text-gray-800 mb-2">
-                            {dataSource.name}
-                          </div>
-                          <div className="absolute top-0 left-auto right-0">
-                            <div className="relative min-w-[2rem] h-8 filter grayscale group-hover:grayscale-0">
-                              <Image
-                                src={`/img/logos/${dataSource.type}.png`}
-                                layout="fill"
-                                alt={`${dataSource.type} data source`}
-                              />
-                            </div>
-                          </div>
-                          <br />
-                        </div>
-                      </PageWrapper.Block>
-                    ))}
+                    You don't have any data sources yet.{" "}
+                    <Link href="/data-sources/new">
+                      <a className="text-blue-600 underline">Add one</a>
+                    </Link>{" "}
+                    now.
                   </>
-                </PageWrapper.Blocks>
+                )}
+
+                {hasDataSources && (
+                  <DataSourcesListing
+                    dataSources={dataSources}
+                    isLoading={isLoading}
+                  />
+                )}
               </div>
             );
           })}
