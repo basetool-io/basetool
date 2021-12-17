4d5c686e349fbeb717b9904d7ef7bf67549d1ff4
Adrian Marin <adrian@adrianthedev.com>
2021-12-17 15:21:14 +0200

    add prefetch

--- features/data-sources/components/DataSourcesBlock.tsx
+++ features/data-sources/components/DataSourcesBlock.tsx
@@ -1,5 +1,6 @@
 import { DataSource, Organization } from "@prisma/client";
 import { useGetDataSourcesQuery } from "../api-slice";
+import { usePrefetch } from "@/features/tables/api-slice";
 import { useProfile } from "@/hooks";
 import Image from "next/image";
 import Link from "next/link";
@@ -13,52 +14,56 @@ const DataSourcesListing = ({
 }: {
   dataSources: DataSource[];
   isLoading: boolean;
-}) => (
-  <PageWrapper.Blocks>
-    <>
-      {isLoading && (
-        <PageWrapper.Block href={`#`}>
-          <div className="text-lg font-bold text-gray-800 mb-2">Loading</div>
-          <br />
-          <Shimmer height={20} />
-        </PageWrapper.Block>
-      )}
+}) => {
+  const prefetchTables = usePrefetch("getTables");
 
-      {dataSources.map((dataSource: DataSource) => (
-        <PageWrapper.Block
-          href={`/data-sources/${dataSource.id}`}
-          key={dataSource.id}
-        >
-          <div className="relative flex flex-col justify-between h-full group">
-            <div className="text-lg font-bold text-gray-800 mb-2">
-              {dataSource.name}
-            </div>
-            <div className="absolute top-0 left-auto right-0">
-              <div className="relative min-w-[2rem] h-8 filter grayscale group-hover:grayscale-0">
-                <Image
-                  src={`/img/logos/${dataSource.type}.png`}
-                  layout="fill"
-                  alt={`${dataSource.type} data source`}
-                />
-              </div>
-            </div>
+  return (
+    <PageWrapper.Blocks>
+      <>
+        {isLoading && (
+          <PageWrapper.Block href={`#`}>
+            <div className="text-lg font-bold text-gray-800 mb-2">Loading</div>
             <br />
-          </div>
-        </PageWrapper.Block>
-      ))}
-    </>
-  </PageWrapper.Blocks>
-);
+            <Shimmer height={20} />
+          </PageWrapper.Block>
+        )}
+
+        {dataSources.map((dataSource: DataSource) => (
+          <PageWrapper.Block
+            href={`/data-sources/${dataSource.id}`}
+            key={dataSource.id}
+            onMouseOver={() =>
+              prefetchTables({
+                dataSourceId: dataSource.id.toString(),
+              })
+            }
+          >
+            <div className="relative flex flex-col justify-between h-full group">
+              <div className="text-lg font-bold text-gray-800 mb-2">
+                {dataSource.name}
+              </div>
+              <div className="absolute top-0 left-auto right-0">
+                <div className="relative min-w-[2rem] h-8 filter grayscale group-hover:grayscale-0">
+                  <Image
+                    src={`/img/logos/${dataSource.type}.png`}
+                    layout="fill"
+                    alt={`${dataSource.type} data source`}
+                  />
+                </div>
+              </div>
+              <br />
+            </div>
+          </PageWrapper.Block>
+        ))}
+      </>
+    </PageWrapper.Blocks>
+  );
+};
 
 const DataSourcesBlock = () => {
   const { data: dataSourcesResponse, isLoading: dataSourcesAreLoading } =
     useGetDataSourcesQuery();
   const { organizations, isLoading: profileIsLoading } = useProfile();
-
-  const isLoading = useMemo(
-    () => dataSourcesAreLoading || profileIsLoading,
-    [dataSourcesAreLoading, profileIsLoading]
-  );
 
   const isLoading = useMemo(
     () => dataSourcesAreLoading || profileIsLoading,
